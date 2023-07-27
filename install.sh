d95b217258940babcf2f9049bc186077dddd9e91
Saint <saint@st-mini.lan>
2023-07-27 14:38:28 -0400

    added karabiner

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ ln -s -f $(greadlink -f mpv) ~/.config/
 ln -s -f $(greadlink -f nvim) ~/.config/
 ln -s -f $(greadlink -f tmux/.tmux.conf) ~/.tmux.conf
 ln -s -f $(greadlink -f zsh/.zshrc) ~/
+ln -s -f $(greadlink -f karabiner/karabiner.json) ~/.config/karabiner/
