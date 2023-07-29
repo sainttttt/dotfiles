6d4e58d943c497d327be8292fda151209a9256c4
saint <sainttttt@x265.ca>
2023-07-29 12:44:56 -0400

    added espanso

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ ln -s -f $(greadlink -f nvim) ~/.config/
 ln -s -f $(greadlink -f tmux/.tmux.conf) ~/.tmux.conf
 ln -s -f $(greadlink -f zsh/.zshrc) ~/
 ln -s -f $(greadlink -f karabiner/karabiner.json) ~/.config/karabiner/
+ln -s -f $(greadlink -f espanso/base.yml) ~/Library/Application\ Support/espanso/match/
