aa56ac4340037d711370cf1fb1ecdc4147b77a54
saint <sainttttt@x265.ca>
2023-12-06 15:59:10 -0500

    updated install file

--- install.sh
+++ install.sh
@@ -4,5 +4,8 @@ ln -s -f $(greadlink -f mpv) ~/.config/
 ln -s -f $(greadlink -f nvim) ~/.config/
 ln -s -f $(greadlink -f tmux/.tmux.conf) ~/.tmux.conf
 ln -s -f $(greadlink -f zsh/.zshrc) ~/
-ln -s -f $(greadlink -f karabiner/karabiner.json) ~/.config/karabiner/
+# ln -s -f $(greadlink -f karabiner/karabiner.json) ~/.config/karabiner/
 ln -s -f $(greadlink -f espanso/base.yml) ~/Library/Application\ Support/espanso/match/
+
+mkdir -p ~/.config/git
+ln -s -f $(greadlink -f git/config) ~/.config/git/
