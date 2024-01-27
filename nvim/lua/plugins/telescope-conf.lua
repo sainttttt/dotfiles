d37cf96b52be3f4a1108800995c0d3cdec34c5eb
saint <sainttttt@x265.ca>
2024-01-27 14:41:20 -0500

    tweaks for file selector truncation

--- nvim/lua/plugins/telescope-conf.lua
+++ nvim/lua/plugins/telescope-conf.lua
@@ -126,6 +126,44 @@ return
             no_header             = false,    -- hide grep|cwd header?
             no_header_i           = false,    -- hide interactive header?
           },
+          files = {
+            -- previewer      = "bat",          -- uncomment to override previewer
+            -- (name from 'previewers' table)
+            -- set to 'false' to disable
+            prompt            = 'Files❯ ',
+            multiprocess      = true,           -- run command in a separate process
+            git_icons         = true,           -- show git icons?
+            file_icons        = true,           -- show file icons?
+            color_icons       = true,           -- colorize file|git icons
+            -- path_shorten   = 1,              -- 'true' or number, shorten path?
+            -- executed command priority is 'cmd' (if exists)
+            -- otherwise auto-detect prioritizes `fd`:`rg`:`find`
+            -- default options are controlled by 'fd|rg|find|_opts'
+            -- NOTE: 'find -printf' requires GNU find
+            -- cmd            = "find . -type f -printf '%P\n'",
+            find_opts         = [[-type f -not -path '*/\.git/*' -printf '%P\n']],
+            rg_opts           = "--color=never --files --hidden --follow -g '!.git'",
+            fd_opts           = "--color=never --type f --hidden --follow --exclude .git",
+            -- by default, cwd appears in the header only if {opts} contain a cwd
+            -- parameter to a different folder than the current working directory
+            -- uncomment if you wish to force display of the cwd as part of the
+            -- query prompt string (fzf.vim style), header line or both
+            -- cwd_header = true,
+            cwd_prompt             = true,
+            cwd_prompt_shorten_len = 10,        -- shorten prompt beyond this length
+            cwd_prompt_shorten_val = 1,         -- shortened path parts length
+            toggle_ignore_flag = "--no-ignore", -- flag toggled in `actions.toggle_ignore`
+            actions = {
+              -- inherits from 'actions.files', here we can override
+              -- or set bind to 'false' to disable a default action
+              --   ["default"]   = actions.file_edit,
+              -- custom actions are available too
+              --   ["ctrl-y"]    = function(selected) print(selected[1]) end,
+              -- action to toggle `--no-ignore`, requires fd or rg installed
+              --   ["ctrl-g"]    = { actions.toggle_ignore },
+            }
+          },
+
 
 
         })
