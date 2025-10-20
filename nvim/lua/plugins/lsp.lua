4f122687ccde7b772b5013fc1889bfb0fe270a73
Saint <sainttttt@x265.ca>
2025-10-20 13:06:19 -0400

    fixed vim keyword in nvim lua This updates the lsp config stuff for the 0.11 or 0.12 (which one?) config format But also fixes the vim global keyword not being detected. Not totally sure why it wasn 't working in the old format, but anyways it seems to be working now so that's sorta two birds in one stone

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,14 +1,5 @@
 -- local completion_capabilities = require('cmp_nvim_lsp').default_capabilities();
 
-vim.lsp.config("lua_ls", {
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { "vim" }}}}})
-
-
-
-
 local on_attach = function(client, bufnr)
   client.server_capabilities.hover = false
   vim.lsp.handlers["textDocument/hover"] = false
@@ -79,10 +70,10 @@ return {
       -- Use an on_attach function to only map the following keys
       -- after the language server attaches to the current buffer
 
-      -- require'lspconfig'.nim_langserver.setup{
-      --   on_attach = on_attach,
-      -- }
-      --
+      require'lspconfig'.nim_langserver.setup{
+        on_attach = on_attach,
+      }
+
 
       local lspconfig = require 'lspconfig'
 
@@ -106,9 +97,9 @@ return {
         end
       }
 
-      lspconfig.nimls.setup {
-        on_attach = on_attach,
-      }
+      -- lspconfig.nimls.setup {
+      --   on_attach = on_attach,
+      -- }
 
       lspconfig.pyright.setup {
         on_attach = on_attach,
@@ -119,38 +110,61 @@ return {
         -- handlers = { ["textDocument/hover"] = function() end }
       }
 
-      lspconfig.lua_ls.setup {
-        on_attach = on_attach,
-        handlers = {
-          ['textDocument/definition'] = function(err, result, ctx, config)
-            if type(result) == 'table' then result = { result[1] } end
-            vim.lsp.handlers['textDocument/definition'](err, result, ctx, config)
-          end,
-        },
+
+      vim.lsp.config('lua_ls', {
+        on_init = function(client)
+          if client.workspace_folders then
+            local path = client.workspace_folders[1].name
+            if
+              path ~= vim.fn.stdpath('config')
+              and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+            then
+              return
+            end
+          end
+
+          client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+            runtime = {
+              -- Tell the language server which version of Lua you're using (most
+              -- likely LuaJIT in the case of Neovim)
+              version = 'LuaJIT',
+              -- Tell the language server how to find Lua modules same way as Neovim
+              -- (see `:h lua-module-load`)
+              path = {
+                'lua/?.lua',
+                'lua/?/init.lua',
+              },
+            },
+            -- Make the server aware of Neovim runtime files
+            workspace = {
+              checkThirdParty = false,
+              library = {
+                vim.env.VIMRUNTIME
+                -- Depending on the usage, you might want to add additional paths
+                -- here.
+                -- '${3rd}/luv/library'
+                -- '${3rd}/busted/library'
+              }
+              -- Or pull in all of 'runtimepath'.
+              -- NOTE: this is a lot slower and will cause issues when working on
+              -- your own configuration.
+              -- See https://github.com/neovim/nvim-lspconfig/issues/3189
+              -- library = {
+              --   vim.api.nvim_get_runtime_file('', true),
+              -- }
+            }
+          })
+        end,
         settings = {
           Lua = {
-            runtime = {
-              -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-              version = 'LuaJIT',
-            },
             diagnostics = {
               -- Get the language server to recognize the `vim` global
               globals = { 'vim' },
               disable = { "redefined-local", "missing-parameter", "redundant-parameter" },
             },
-            workspace = {
-              -- Make the server aware of Neovim runtime files
-              library = vim.api.nvim_get_runtime_file("", true),
-              checkThirdParty = false,
-            },
-            -- Do not send telemetry data containing a randomized but unique identifier
-            telemetry = {
-              enable = false,
-            },
-          },
-
-        },
-      }
+          }
+        }
+      })
 
       lspconfig.sourcekit.setup {
         on_attach = on_attach,
