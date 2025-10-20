f1ef09bcc37e2e80f3108b26e7c9a9d37448598e
Saint <sainttttt@x265.ca>
2025-10-20 13:07:06 -0400

    Fixes to Todo plugin Got it so that it deletes the task once it's done and some other fixes

--- nvim/lua/todo.lua
+++ nvim/lua/todo.lua
@@ -1,3 +1,10 @@
+-- dailies root path
+local dailies_root = "/Users/saint/Library/Mobile Documents/iCloud~md~obsidian/Documents/DAY"
+
+-- the filename of the daily filename
+local daily_filename = string.format("%s/%s-daily.md", dailies_root, os.date("%Y-%m-%d"))
+
+
 function reverse_in_place(list)
     local n = #list
     for i = 1, math.floor(n / 2) do
@@ -51,14 +58,7 @@ local function insert_at_final_match_or_add_missing(string_list, new_line, filen
 
   cursor = last_match + 1
 
-
-  print("here")
-  print(cursor)
-  -- print(#lines + 1)
-  print("here2")
-
   if cursor == (#lines + 1) and cursor ~= 1 then
-    print("hereeeee")
     table.insert(lines, cursor, "\n")
     cursor = cursor + 1
   end
@@ -78,50 +78,20 @@ local function insert_at_final_match_or_add_missing(string_list, new_line, filen
   file:close()
 end
 
-local function insert_at_final_match(string_list, new_line, filename)
-  -- Read the file into a lines table
-  local lines = {}
-  for line in io.lines(filename) do
-    table.insert(lines, line)
-  end
+local function complete_todo()
+  -- this is the line that we're gonna insert as the task done
+  -- todo: can we expand this for multi line tasks?
+  -- for the time let's just stick to one line tasks
+  local line_text = vim.api.nvim_get_current_line()
 
-  -- Start from beginning, searching for each string in order
-  local cursor = 1
-  for _, search_str in ipairs(string_list) do
-    local found = false
-    while cursor <= #lines do
-      if lines[cursor]:find(search_str, 1, true) then
-        found = true
-        break
-      end
-      cursor = cursor + 1
-    end
-    if not found then
-      error("String not found: " .. search_str)
-    end
-    -- move cursor to the line after the match for next search
-    cursor = cursor + 1
-  end
+  local orig_line = vim.api.nvim_win_get_cursor(0)
+  print("orign lne")
+  print(orig_line)
 
-  -- Insert new_line at cursor position
-  table.insert(lines, cursor, new_line)
-
-  -- Write back to file
-  local file = io.open(filename, "w")
-  if not file then error("Cannot open file for writing: " .. filename) end
-  for _, line in ipairs(lines) do
-    file:write(line .. "\n")
-  end
-  file:close()
-end
-
-
-local dailies_root = "/Users/saint/Library/Mobile Documents/iCloud~md~obsidian/Documents/DAY"
-
-
-local function jump_to_less_indent()
-  local lineText = vim.api.nvim_get_current_line()
+  -- the list of sub headers of the tasks which we will
+  -- insert into the daily file
   local headers = {}
+
   local prev_line = -1
   local current_line = -2
   while current_line ~= prev_line do
@@ -130,44 +100,25 @@ local function jump_to_less_indent()
       table.insert(headers, line)
     end
     prev_line = vim.api.nvim_win_get_cursor(0)[1]
+
+    -- This is the vindent.nvim dependecy, maybe this can be
+    -- deprecated in the future, like rip it out of the library
+    -- or write our own? Not sure
     require("vindent-core").Motion("prev", true, "less", "n", 1)
     current_line = vim.api.nvim_win_get_cursor(0)[1]
   end
-  print(dump(headers))
+
+  -- we have to reverse the list of headers we get to write it properly
+  -- nothing weird really lol
   reverse_in_place(headers)
 
-
-
-  local daily_filename = string.format("%s/%s-daily.md", dailies_root, os.date("%Y-%m-%d"))
   create_blank_if_not_exists(daily_filename)
-  insert_at_final_match_or_add_missing(headers, lineText, daily_filename)
+  insert_at_final_match_or_add_missing(headers, line_text, daily_filename)
+
+  vim.api.nvim_win_set_cursor(0, orig_line)
+
+  vim.api.nvim_feedkeys("dd", "n", false)
 end
 
-vim.keymap.set('n', '<leader>kk', jump_to_less_indent, { desc = "Jump to previous line with less indentation" })
-
-
-vim.keymap.set('n', '<leader>kl', jump_to_less_indent, { desc = "Jump to previous line with less indentation" })
--- --- --- ---
--- Example Usage
--- --- --- ---
-
--- Imagine you have a file "config.txt" with the content:
--- setting_a = 1
--- setting_b = 2
-
--- Scenario 1: The target text exists.
--- find_and_append_in_background("config.txt", "setting_a", "# This is a comment for setting_a")
--- Resulting buffer content for "config.txt":
--- setting_a = 1
--- # This is a comment for setting_a
--- setting_b = 2
-
--- Scenario 2: The target text does NOT exist.
--- find_and_append_in_background("config.txt", "setting_c", "setting_c = 3")
--- Resulting buffer content for "config.txt" (assuming the previous change was saved):
--- setting_a = 1
--- # This is a comment for setting_a
--- setting_b = 2
--- setting_c
--- setting_c = 3
+vim.keymap.set('n', '<leader>kk', complete_todo, { desc = "Jump to previous line with less indentation" })
 
