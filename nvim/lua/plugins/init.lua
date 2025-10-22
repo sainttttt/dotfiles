cc2bb13094214c57204881e73562c2eeb430e27e
Saint <sainttttt@x265.ca>
2025-10-22 11:49:18 -0400

    config stuff for screenkey this stuff will help for screencasts to show what key is being pressed added some settings

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -388,11 +388,94 @@ return {
 
 
     },
-},
+  },
 
   -- displays pressed keys for screencast
   { 'NStefan002/screenkey.nvim',
     config = function()
+      require("screenkey").setup({
+        win_opts = {
+          row = vim.o.lines - vim.o.cmdheight - 1,
+          col = vim.o.columns - 1,
+          relative = "editor",
+          anchor = "SE",
+          width = 20,
+          height = 3,
+          border = "single",
+          title = "†",
+          title_pos = "center",
+          style = "minimal",
+          focusable = false,
+          noautocmd = true,
+        },
+        hl_groups = {
+          ["screenkey.hl.key"] = { link = "Normal" },
+          ["screenkey.hl.map"] = { link = "Normal" },
+          ["screenkey.hl.sep"] = { link = "Normal" },
+        },
+        compress_after = 10,
+        clear_after = 2,
+        emit_events = true,
+        disable = {
+          filetypes = {},
+          buftypes = {},
+          modes = {},
+        },
+        show_leader = true,
+        group_mappings = false,
+        display_infront = {},
+        display_behind = {},
+        filter = function(keys)
+          local ignore = { "<Esc>"}
+          return vim.iter(keys)
+            :filter(function(k)
+              return not vim.tbl_contains(ignore, k.key)
+            end)
+            :totable()
+        end,
+        colorize = function(keys)
+          return keys
+        end,
+        separator = " ",
+        keys = {
+          ["<TAB>"] = "󰌒",
+          ["<CR>"] = "󰌑",
+          ["<ESC>"] = "Esc",
+          ["<SPACE>"] = "␣",
+          ["<BS>"] = "󰌥",
+          ["<DEL>"] = "Del",
+          ["<LEFT>"] = "",
+          ["<RIGHT>"] = "",
+          ["<UP>"] = "",
+          ["<DOWN>"] = "",
+          ["<HOME>"] = "Home",
+          ["<END>"] = "End",
+          ["<PAGEUP>"] = "PgUp",
+          ["<PAGEDOWN>"] = "PgDn",
+          ["<INSERT>"] = "Ins",
+          ["<F1>"] = "󱊫",
+          ["<F2>"] = "󱊬",
+          ["<F3>"] = "󱊭",
+          ["<F4>"] = "󱊮",
+          ["<F5>"] = "󱊯",
+          ["<F6>"] = "󱊰",
+          ["<F7>"] = "󱊱",
+          ["<F8>"] = "󱊲",
+          ["<F9>"] = "󱊳",
+          ["<F10>"] = "󱊴",
+          ["<F11>"] = "󱊵",
+          ["<F12>"] = "󱊶",
+          ["CTRL"] = "Ctrl",
+          ["ALT"] = "Alt",
+          ["SUPER"] = "󰘳",
+          ["<leader>"] = "<leader>",
+        },
+        notify_method = "echo",
+        log = {
+          min_level = vim.log.levels.OFF,
+          filepath = vim.fn.stdpath("data") .. "/screenkey_log",
+        },
+      })
     end
   },
 
